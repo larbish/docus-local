67db91ee8320ae16ffdb74a2560a1ea2d166b04a
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-06-26 19:05:09 +0200

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,7 @@ import { defineNuxtConfig } from "nuxt/config"
 export default defineNuxtConfig({
     content: {
         preview: {
-            dev: true, 
-            api: 'http://localhost:1337',
-            gitInfo: {
-                owner: 'larbish',
-                name: 'docus-v3'
-            }
+            api: 'https://api.nuxt.studio',
         }
     }
 });
